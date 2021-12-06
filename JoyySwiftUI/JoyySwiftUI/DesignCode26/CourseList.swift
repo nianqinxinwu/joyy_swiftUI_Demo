66289f10d948f3cd409fafaf8aadc839ee3b55d6
changyuan1 <changyuan1@joyy.com>
2021-12-06 14:28:37 +0800

    SwiftUI_Demo_001

--- JoyySwiftUI/JoyySwiftUI/DesignCode26/CourseList.swift
+++ JoyySwiftUI/JoyySwiftUI/DesignCode26/CourseList.swift
@@ -9,28 +9,51 @@ import SwiftUI
 
 struct CourseList: View {
     @State var courses = courseData
+    @State var active = false
+    @State var activeIndex = -1
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 30) {
-                Text("Courses")
-                    .font(.largeTitle).bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 30)
-                    .padding(.top, 30)
-                
-                ForEach(courses.indices, id: \.self) { index in
-                    GeometryReader { geometry in
-                        CourseView(show: self.$courses[index].show, course: courses[index])
-                            .offset(y: self.courses[index].show ? -geometry.frame(in:.global).minY : 0)
+        ZStack {
+            Color.black.opacity(active ? 0.5 : 0)
+                .animation(.linear)
+                .edgesIgnoringSafeArea(.all)
+            ScrollView {
+                VStack(spacing: 30) {
+                    /// 标题
+                    Text("Courses")
+                        .font(.largeTitle).bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                        .padding(.top, 30)
+                        .blur(radius: active ? 20 : 0)
+                     
+                    /// 列表
+                    ForEach(courses.indices, id: \.self) { index in
+                        GeometryReader { geometry in
+                            CourseView(
+                                show: self.$courses[index].show,
+                                course: courses[index],
+                                active: $active,
+                                index: index,
+                                activeIndex: $activeIndex
+                            )
+                                .offset(y: self.courses[index].show ? -geometry.frame(in:.global).minY : 0)
+                                .opacity(self.activeIndex != index && self.active ? 0 : 1)
+                                .scaleEffect( self.activeIndex != index && self.active ? 0.5 : 1)
+                                .offset(x: self.activeIndex != index && self.active ? screen.width : 0)
+                        }
+    //                    .frame(height: self.courses[index].show ? screen.height : 280)
+                        .frame(height: 280)
+                    .frame(maxWidth: self.courses[index].show ? .infinity : screen.width - 60)
+                        /// 展示时则将对应的视图展示在顶部
+                    .zIndex(self.courses[index].show ? 1 : 0)
                     }
-//                    .frame(height: self.courses[index].show ? screen.height : 280)
-                    .frame(height: 280)
-                .frame(maxWidth: self.courses[index].show ? .infinity : screen.width - 60)
                 }
+                .frame(width: screen.width)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             }
-            .frame(width: screen.width)
-            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            .statusBar(hidden: active ? true : false)
+            .animation(.linear)
         }
     }
 }
@@ -44,6 +67,11 @@ struct CourseList_Previews: PreviewProvider {
 struct CourseView: View {
     @Binding var show: Bool
     var course: Course
+    @Binding var active: Bool
+    var index : Int
+    @Binding var activeIndex: Int
+    @State var activeView = CGSize.zero
+    
     let viewMaxHeight = 460.0
     var body: some View {
         ZStack(alignment: .top) {
@@ -104,11 +132,27 @@ struct CourseView: View {
             .background(course.color)
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: course.color.opacity(0.3), radius: 20, x: 0, y: 20)
+            .gesture(
+                DragGesture().onChanged({ value in
+                    self.activeView = value.translation
+                })
+                .onEnded({ value in
+                    self.activeView = .zero
+                })
+            )
+            /// 改变展示状态，激活状态
             .onTapGesture {
                 self.show.toggle()
+                self.active.toggle()
+                if self.show {
+                    self.activeIndex = self.index
+                } else {
+                    self.activeIndex = -1
+                }
             }
         }
         .frame(height: show ? screen.height: 280)
+        .scaleEffect(1 - self.activeView.height / 1000)
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
         .edgesIgnoringSafeArea(.all)
     }
